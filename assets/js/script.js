0b592d6e207b52954d51a116323d087bd22188f1
DanielKrems <danielkremes05@gmail.com>
2024-02-17 19:21:52 -0300

    add project

---
+++ assets/js/script.js
@@ -0,0 +1,8 @@
+function toggle() {
+    // change background page
+    document.body.classList.toggle("dark");
+    document.querySelector('.header').classList.toggle("light");
+    document.querySelector('.footer').classList.toggle("light");
+    document.querySelector('.container').classList.toggle("light_theme");
+    
+}
